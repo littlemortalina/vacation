ce311d381cc97367dd71adb2845d011e9d175bdc
Alina <al89056667426@gmail.com>
2025-08-25 17:44:41 +0300

    Task2V2

--- src/test/java/ru/netology/vacation/vacation/services/vacationCalcTest.java
+++ src/test/java/ru/netology/vacation/vacation/services/vacationCalcTest.java
@@ -3,15 +3,13 @@ package ru.netology.vacation.vacation.services;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class vacationCalcTest {
     @ParameterizedTest
     //подготавливаем данные
-    @CsvSource({
-            "3, 10000, 3000, 20000",
-            "2, 100000, 60000, 150000"
-    })
+    @CsvFileSource (files = "src/test/resources/vacation.csv")
     public void testVacationMonths(int expected, int income, int expenses, int threshold) {
         vacationCalc service = new vacationCalc();
 
