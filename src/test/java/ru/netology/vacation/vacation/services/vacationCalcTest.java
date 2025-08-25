fdd61c59cbfc71c021dcfb17e5778f143eecfed7
Alina <al89056667426@gmail.com>
2025-08-25 17:38:44 +0300

    Task2V1

--- src/test/java/ru/netology/vacation/vacation/services/vacationCalcTest.java
+++ src/test/java/ru/netology/vacation/vacation/services/vacationCalcTest.java
@@ -2,42 +2,23 @@ package ru.netology.vacation.vacation.services;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class vacationCalcTest {
-
-    @Test
-    void shouldCalculateWithIncome10_000Expenses3_000Threshold20_000() {
+    @ParameterizedTest
+    //подготавливаем данные
+    @CsvSource({
+            "3, 10000, 3000, 20000",
+            "2, 100000, 60000, 150000"
+    })
+    public void testVacationMonths(int expected, int income, int expenses, int threshold) {
         vacationCalc service = new vacationCalc();
-
-        // подготавливаем данные
-        int income = 10_000; // доход
-        int expenses = 3_000; // траты
-        int threshold = 20_000; // порог остатка
-        int expected = 3; // ожидаемый результат
 
         // вызываем целевой метод
         int actual = service.calculate(income, expenses, threshold);
 
         // сравниваем ожидаемый и фактический результат
         Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    void shouldCalculateWithIncome100_000Expenses60_000Threshold150_000() {
-        vacationCalc service = new vacationCalc();
-
-        // подготавливаем данные
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-
-        // вызываем целевой метод
-        int actual = service.calculate(income, expenses, threshold);
-
-        // сравниваем ожидаемый и фактический результат
-        Assertions.assertEquals(expected, actual);
-
     }
 }
